7b93800c648265e425c79b15571e4deb26fdcba0
Tiffany Hoang <tthuy.hoangt@gmail.com>
2021-11-28 23:14:42 +1100

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
-  extends: ['react-app', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['dist'],
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export const App = () => {
   return <h1 className="text-red-600 text-center text-xl">Hello world!</h1>
 }
